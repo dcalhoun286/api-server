98135348c65a0a8a1627f22656bce835f1ce8341
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2021-07-13 01:03:46 -0700

    deleted unused module and corpse code

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 // mongoose is the connector between our app and the MongoDB database
 const mongoose = require('mongoose');
 const MONGODB_URI='mongodb://localhost:27017/api-server-db';
-const Clothes = require('./src/models/clothesModel.js');
 const options = { useNewUrlParser: true, useUnifiedTopology: true }; // always pass in these values
 require('dotenv').config();
 
@@ -13,17 +12,3 @@ const server = require('./src/server.js');
 mongoose.connect(MONGODB_URI, options)
   .then(() => server.start(server.PORT))
   .catch(err => console.error(err));
-
-// let connectToDB = async () => {
-//   let greyTshirt = {
-//     type: 'shirt',
-//     color: 'grey',
-//   };
-
-//   let greyShirt1 = new Clothes(greyTshirt);
-//   await greyShirt1.save();
-
-//   console.log(greyShirt1);
-// };
-
-// connectToDB();
